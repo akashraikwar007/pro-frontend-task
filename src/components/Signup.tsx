604d24845fa2ec2fd69fb86f6845b7a2110e037e
akashraikwar007 <akashraikwar763@gmail.com>
2025-04-30 13:12:59 +0530

    fix errors and do correct

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -32,7 +32,7 @@ const Signup: React.FC = () => {
     setError("");
     setSuccess("");
     try {
-      const res = await fetch("hhttps://user-table-6fdk.onrender.com/auth/signup", {
+      const res = await fetch("https://user-table-6fdk.onrender.com/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
